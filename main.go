90e38a09e9ef4cbfde587073521e2bc0a85b3726
joelson Silva <joelson.s@outlook.com>
2026-03-15 15:09:23 -0300

    atualizando

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
-import ( 
+import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
-type  Page struct {
+type Page struct {
 	Title string
-	Body []byte 
+	Body  []byte
 }
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+/*func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid Page Title")
+	}
+	return m[2], nil
+}*/
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
@@ -18,55 +32,68 @@ func (p *Page) save() error {
 }
 
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := os.ReadFile(filename)
- if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request)  {
+/*func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}*/
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
-    p, _ := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-        "<form action=\"/save/%s\" method=\"POST\">"+
-        "<textarea name=\"body\">%s</textarea><br>"+
-        "<input type=\"submit\" value=\"Save\">"+
-        "</form>",
-        p.Title, p.Title, p.Body)
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request){
-	title := r.URL.Path[len("/save/"):]
-    body := r.FormValue("body")
-    p := &Page{Title: title, Body: []byte(body)}
-    p.save()
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := templates.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+
+}
+func makeHandle(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
 }
 
-func main()  {
-	p1 := &Page{Title: "TestPage", Body: []byte("this is a sample Page.")}
-	p1.save()
-	p2 := &Page{Title: "Joelson", Body: []byte("this is a Joelson's Page.")}
-	p2.save()
+func main() {
+	//
 	//http.HandleFunc("/", handler)
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-    http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandle(viewHandler))
+	http.HandleFunc("/edit/", makeHandle(editHandler))
+	//http.HandleFunc("/save/", makeHandle(saveHandler))
 	fmt.Println("Server in runnin on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
